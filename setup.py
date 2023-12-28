e6df127815cf62c8b6407cb7ec7ecb0dacc46c83
MarinaVasilevaIVT <99833234+MarinaVasilevaIVT@users.noreply.github.com>
2023-12-28 20:36:04 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MarinettCalculator',
+    version='1.0.0',
+    packages=find_packages(),
+    author='Vasileva Marina',
+    author_email='marinettsan@mail.ru',
+    description='Программа-калькулятор для ваших вычислений',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
